4512faccef7bc050edea68a13e83cac4ed595063
vidhyaramesh117 <vidhyaramesh117@gmail.com>
2023-01-13 16:03:14 +0530

    nav_drawer

--- Toolbar2/app/src/main/java/com/example/toolbar/MainActivity.java
+++ Toolbar2/app/src/main/java/com/example/toolbar/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.toolbar;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
@@ -11,12 +13,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     TabLayout tabLayout;
     ViewPager viewPager;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.nav_drawer);
+        navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.tb);
         tabLayout = findViewById(R.id.tab);
         viewPager = findViewById(R.id.vp);
@@ -36,16 +43,41 @@ public class MainActivity extends AppCompatActivity {
         if(getSupportActionBar()!=null)
         {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setTitle("TOOLBAR");
+            getSupportActionBar().setTitle("WHATSAPP");
         }
-        toolbar.setSubtitle("Sub Title");
+        toolbar.setSubtitle("");
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.OpenDrawer,R.string.CloseDraw);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                int id = item.getItemId();
+                if(id==R.id.profile)
+                {
+
+                }
+                else if(id==R.id.log)
+                {
+
+                }
+                else
+                {
+                    
+                }
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         new MenuInflater(this).inflate(R.menu.opt_menu,menu);
-        return super.onCreateOptionsMenu(menu);
+      return super.onCreateOptionsMenu(menu);
     }
 
     @Override
