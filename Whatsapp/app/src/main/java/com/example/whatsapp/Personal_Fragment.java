68135451f0b588be37148a039f72390c9ce8fc2d
vidhyaramesh117 <vidhyaramesh117@gmail.com>
2023-01-19 15:59:52 +0500

    trail

---
+++ Whatsapp/app/src/main/java/com/example/whatsapp/Personal_Fragment.java
@@ -0,0 +1,26 @@
+package com.example.whatsapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Personal_Fragment extends Fragment {
+
+
+
+    public Personal_Fragment()
+    {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_personal_, container, false);
+    }
+}
