93423fab6789ff33ab79c1da110b817eaf8dd2f1
vidhyaramesh117 <vidhyaramesh117@gmail.com>
2023-01-20 15:38:07 +0500

    full project till toolbar icon activity

---
+++ Toolbar2/app/src/main/java/com/example/toolbar/chat_Fragment.java
@@ -0,0 +1,25 @@
+package com.example.toolbar;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class chat_Fragment extends Fragment
+{
+    public chat_Fragment()
+    {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_chat_, container, false);
+    }
+}
